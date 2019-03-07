e2b190d65e9336e0a5206928925a8f1616012b2a
Ava_virtual <1207260077@qq.com>
2019-03-07 15:56:13 +0800

    change gitignore

--- cpp_3_7.cpp
+++ cpp_3_7.cpp
@@ -4,6 +4,8 @@
 
 int main(int argc, char const *argv[])
 {
+    int a = argc;
+    std::cout << a << std::endl;
     std::string str = "nihao";
     std::cout << "hello world" << std::endl;
     std::cout << str << std::endl;
