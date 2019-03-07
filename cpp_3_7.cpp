25d4522c452ef882dfdc9e2847e64c3d542dd19f
Ava_virtual <1207260077@qq.com>
2019-03-07 10:51:24 +0800

    add .gitignore

---
+++ cpp_3_7.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+
+int main(int argc, char const *argv[])
+{
+    std::string str = "nihao";
+    std::cout << "hello world" << std::endl;
+    std::cout << str << std::endl;
+    return 0;
+}
