cd3e8c2c00d6a83d7fb26d994fd74912e6a731d7
Ava_virtual <1207260077@qq.com>
2019-03-08 17:40:32 +0800

    3_8 back to home now

--- cpp_3_8_chapter1.cpp
+++ cpp_3_8_chapter1.cpp
@@ -2,10 +2,17 @@
 
 int main(int argc, char const *argv[])
 {
-    int value = 0, sum = 0;
-    while(std::cin >> value){
-        sum += value;
-    }
-    std::cout << "sum = " << sum << std::endl;
+    // int value = 0, sum = 0;
+    // while(std::cin >> value){
+    //     sum += value;
+    // }
+    // std::cout << "sum = " << sum << std::endl;
+
+    int a{1};
+    int &b{a};
+    int *p{&a};
+    int &q{*p};
+
+    std::cout << b << std::endl;
     return 0;
 }
