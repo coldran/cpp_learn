1d2eecf10c05c834eeb5763822a2100be9f29b5e
Ava_virtual <1207260077@qq.com>
2019-03-09 18:03:28 +0800

    learn about const & constexpr

--- cpp_3_8_chapter1.cpp
+++ cpp_3_8_chapter1.cpp
@@ -8,10 +8,29 @@ int main(int argc, char const *argv[])
     // }
     // std::cout << "sum = " << sum << std::endl;
 
-    int a{1};
-    int &b{a};
-    int *p{&a};
-    int &q{*p};
-    std::cout << b << std::endl;
+    // int a{1};
+    // int &b{a};
+    // int *p{&a};
+    // int &q{*p};
+    // std::cout << b << std::endl;
+
+    int i{10};
+    const int &j{i};
+    int  &k{i};
+    std::cout << i << '\t' << j << '\t' << k << std::endl;
+    k = 5;
+    std::cout << i << '\t' << j << '\t' << k << std::endl;
+
+    int i2{0};
+    const int *const l{&i2};
+    printf("%d",*l);
+
+    int n;
+    std::cin >> n;
+    constexpr int m = 3;
+    std::cout << m << std::endl;
+
+    constexpr int *c{0};
+
     return 0;
 }
